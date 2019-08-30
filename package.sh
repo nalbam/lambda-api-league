ebdcf4b010aea4b1159335a05921ec039e6023e9
nalbam <me@nalbam.com>
2019-08-30 18:27:10 +0900

    CIRCLE_BUILDER

--- package.sh
+++ package.sh
@@ -1,13 +1,41 @@
 #!/bin/bash
 
-rm -rf target
-mkdir -p target
+CMD=${1:-$CIRCLE_JOB}
 
-cp ./*.tf ./target/
+CIRCLE_BUILDER=${CIRCLE_BUILDER}
 
-pushd src
+_build() {
+    rm -rf target
+    mkdir -p target
 
-npm run build
-zip -r ../target/lambda.zip *
+    cp ./*.tf ./target/
 
-popd
+    pushd src
+
+    npm run build
+    zip -r ../target/lambda.zip *
+
+    popd
+}
+
+_test() {
+    terraform init
+    terraform plan -var "build_no=$CIRCLE_BUILDER"
+}
+
+_deploy() {
+    terraform init
+    terraform apply -var "build_no=$CIRCLE_BUILDER" -auto-approve
+}
+
+case ${CMD} in
+    build)
+        _build
+        ;;
+    test)
+        _test
+        ;;
+    deploy)
+        _deploy
+        ;;
+esac
