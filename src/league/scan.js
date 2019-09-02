87b8d1f1de6e3ce14f66ccba0b58d7732bcbe1fb
nalbam <me@nalbam.com>
2019-09-02 10:41:44 +0900

    add logo

--- src/league/scan.js
+++ src/league/scan.js
@@ -56,6 +56,7 @@ module.exports.scan = (event, context, callback) => {
         let body = {
             league: result.Item.league,
             title: result.Item.title,
+            logo: result.Item.logo,
             items: []
         }
 
